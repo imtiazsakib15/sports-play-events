b27952e04ff982107935ba8d79da58d615ea276f
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-10-07 21:14:02 +0600

    banner section added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,39 @@
+// import bannerImg from "./../assets/banner.jpg";
+
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  return (
+    <div className="2xl:container mx-auto">
+      <section className=" bg-[url(https://i.ibb.co/H2RQbpb/banner.jpg)] bg-cover bg-center bg-no-repeat text-black">
+        <div className="bg-gray-100 opacity-90">
+          <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+            <div className="mx-auto max-w-4xl text-center">
+              <h1 className="text-3xl/tight sm:text-4xl/tight font-extrabold lg:text-5xl/snug px-2">
+                Creating Unforgettable Sporting Experiences, Every Time
+              </h1>
+
+              <p className="mx-auto mt-4 max-w-2xl px-6 sm:text-xl/relaxed">
+                We are passionate about sports and dedicated to delivering
+                flawless event management services. From tournaments and leagues
+                to tailored experiences, we blend expertise with enthusiasm to
+                create events that leave a lasting impact.
+              </p>
+
+              <div className="mt-8 flex flex-wrap justify-center gap-4">
+                <Link
+                  className="block rounded border border-blue-700 bg-blue-700 px-8 py-2 font-medium text-white focus:outline-none focus:ring active:text-opacity-75 w-auto"
+                  to="/login"
+                >
+                  Connected With Us
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,9 @@
+import Banner from "../components/Banner";
+
 const Home = () => {
   return (
     <div className="">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit beatae
-        cum vitae odit amet ratione tempora, at aspernatur illum aperiam unde
-        commodi eligendi tempore fugiat? Libero, sit. Blanditiis, maiores
-        aliquid necessitatibus harum, sit voluptas quibusdam provident officia a
-        qui nam nesciunt. Adipisci sequi, necessitatibus, ipsa laudantium
-        officia soluta harum assumenda dolorem voluptatibus iusto neque enim
-        itaque at corporis dicta impedit dolore eveniet ullam dolorum. Magnam
-        aliquid quas quam esse quia eius, commodi officiis maiores nemo harum
-        similique dolores, nobis sunt! Aliquam iste voluptatem odit repellat
-        facere, laudantium consectetur error placeat nulla commodi. Blanditiis,
-        officia commodi adipisci iste quidem incidunt, voluptate doloremque in
-        at a nesciunt dolorem, odit accusantium eveniet architecto deleniti?
-        Unde alias dolore, debitis consequuntur quis perferendis inventore, odio
-        exercitationem eveniet quia beatae rerum doloribus mollitia consequatur
-        maiores, quo illo illum. Eos molestias voluptates corporis, pariatur
-        magni cupiditate dolorum, distinctio velit iure esse debitis rerum quod
-        atque amet aperiam tempora, nisi delectus perspiciatis vel inventore
-        itaque nam ratione voluptas. Quasi velit nesciunt, repellendus sint
-        deleniti saepe eaque odio provident ipsa natus praesentium. Molestiae
-        eligendi ipsum odio totam veritatis asperiores adipisci nihil eum vitae
-        cupiditate, ut quisquam placeat obcaecati, reprehenderit eius, excepturi
-        veniam accusantium blanditiis id maxime quasi error perspiciatis.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius accusamus accusantium reiciendis sequi, in nemo quidem dolor consequuntur ad aperiam. Odio accusantium maxime architecto soluta iusto error dolorum voluptatibus culpa omnis blanditiis debitis a praesentium cumque deleniti ipsum, asperiores quia neque similique ex aperiam! Laudantium, dolores. Facere porro ad a doloremque, ex corrupti vero minus totam error quisquam animi, deserunt quasi voluptatem inventore! Commodi, accusamus rerum! Veritatis, nisi doloribus cum non ut corrupti esse. Quidem quam dolores voluptas beatae? Ut qui non, id atque distinctio, commodi debitis explicabo et voluptatibus fugiat aut ullam corrupti nihil obcaecati itaque. Sequi modi ullam omnis eius recusandae rerum, perferendis nulla explicabo saepe quibusdam. Iure error necessitatibus porro praesentium consectetur tempora inventore vero cupiditate quidem obcaecati aliquam iusto quos impedit delectus accusamus nemo, dicta dignissimos eveniet vitae animi soluta asperiores? Architecto reiciendis aperiam enim tempore saepe optio quisquam asperiores, cumque recusandae sed voluptas earum? Pariatur, nostrum nulla! Consequatur illo dolor laboriosam repellendus impedit nihil rem, commodi aperiam quae. Tenetur quis quos ullam fugiat ratione sint mollitia, aperiam voluptatem consectetur blanditiis cum nihil, autem ipsum temporibus libero, saepe voluptates quidem amet est. Molestias fugit explicabo cupiditate fugiat possimus omnis hic modi maxime, ipsam corrupti, deserunt tenetur? Aliquam expedita ea repudiandae nobis officia perspiciatis molestias dolorem veniam quam, fugiat nesciunt odio numquam atque necessitatibus dicta, tempora reiciendis vel aut reprehenderit sint maiores dignissimos, distinctio excepturi voluptatibus. Cupiditate perferendis rem soluta amet fugit repellat eligendi saepe voluptatem, pariatur laudantium eos. Praesentium blanditiis quae quos, similique eaque explicabo architecto illo sunt harum, et, fugit doloribus fugiat nostrum! Architecto unde dolor blanditiis fugiat ipsa magnam obcaecati cum praesentium optio totam inventore repellat rem, reiciendis ad. Dolor architecto dicta, impedit quasi repellat excepturi aspernatur repudiandae odio ut nihil debitis sequi modi soluta magnam! Repudiandae impedit eum libero ducimus, fuga quos! Obcaecati inventore, consequatur esse nemo eveniet minima quas animi magnam nobis placeat rem quibusdam illo perspiciatis harum laborum ducimus aspernatur! Reprehenderit minus ut exercitationem laudantium inventore, totam, expedita dolor veniam voluptate quod debitis eaque eos accusantium in architecto. Rem excepturi minus voluptas quisquam quasi cumque consequatur recusandae perferendis vel odio maiores optio ratione animi dolore soluta asperiores, saepe voluptates ab vero distinctio aliquam. Nisi beatae consequatur tempora fugit alias neque ipsa. Recusandae a repudiandae cumque doloremque. Voluptate numquam itaque deserunt reiciendis voluptatibus optio officiis eos atque est fugiat? Quaerat officiis eaque quibusdam. Voluptate, at adipisci? Aut, incidunt voluptate. Iure, nisi? Perspiciatis non dolor quia natus, recusandae animi a sint asperiores provident ratione, molestiae illo nihil explicabo. Quas omnis aliquid, accusamus obcaecati, dolore nostrum iusto ipsa voluptatum corrupti explicabo maxime eum nihil eos id aperiam provident sit consequatur dolor ex sed illum alias, rerum minus aspernatur? Voluptatibus, quasi porro corrupti repudiandae minus amet omnis? Vero iusto sint tempora eum perspiciatis corporis a id reiciendis ut aliquam architecto, porro aspernatur iste, mollitia dolorem. Mollitia est deserunt aspernatur corrupti numquam natus. Totam voluptates fuga veniam quod facilis nemo consequatur aliquam id cum. Adipisci quae id repudiandae, nesciunt a hic laudantium quos minima error autem.
-      </p>
+        <Banner/>
     </div>
   );
 };
